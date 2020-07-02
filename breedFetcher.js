08a0d7a3adc64d74c1ddd2d619840b381b63fb75
Dane Mancuso <danegmancuso@gmail.com>
2020-07-02 21:55:51 +0000

    added cat API functionality with test cases

---
+++ breedFetcher.js
@@ -0,0 +1,32 @@
+const request = require('request');
+// console.log(request)
+const arg = process.argv.splice(2);
+
+
+const baseUrl = 'https://api.thecatapi.com/v1/breeds/search';
+const addToUrl = `?q=${arg[0]}`;
+const fullUrl = baseUrl + addToUrl;
+
+
+request(fullUrl, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  // console.log(typeof body)
+
+  if (error) {
+    return console.log('error ', error);
+  }
+
+  const data = JSON.parse(body);
+  // console.log(data)
+  if (data.length > 0) {
+    console.log(data[0].description);
+  } else {
+    console.log('Cats not found 🥺');
+  }
+
+
+
+
+});
